9ff2ce061feb82dc0ace6b4e343ae9c86cc8bcf9
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2021-01-27 21:20:29 +0100

    added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bitchute", # Replace with your own username
+    version="0.0.1",
+    author="Marcus Burkhardt",
+    author_email="marcus.burkhardt@gmail.com",
+    description="A package to scrape bitchute.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bumatic/bitchute-scraper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires = ['beautifulsoup4', 'markdownify', 'pandas', 'python-dateutil', 'retrying', 'selenium', 'tqdm', 'webdriver-manager'],
+)
