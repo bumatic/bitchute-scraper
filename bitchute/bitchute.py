396d9efd8ba7ff6b0f4e4e70b5942e3515618267
bumatic <marcus.burkhardt@gmail.com>
2025-07-11 16:43:07 +0200

    Update bitchute.py

--- bitchute/bitchute.py
+++ bitchute/bitchute.py
@@ -15,15 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
-# Enhanced Bitchute Scraper with improved architecture and error handling
-# Copyright (C) 2025 Enhanced by Claude
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
 import time
 import logging
 import json
