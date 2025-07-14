544aabb52174a5d956db8799ff27f11c3fafcfc6
bumatic <marcus.burkhardt@gmail.com>
2025-07-14 07:25:45 +0200

    Update __init__.py

--- bitchute/__init__.py
+++ bitchute/__init__.py
@@ -8,7 +8,7 @@ from .bitchute import BitChuteAPI, Video, Channel, Hashtag, TokenManager
 __version__ = "2.0.0"
 __author__ = "Marcus Burkhardt"
 __email__ = "marcus.burkhardt@gmail.com"
-__description__ = "Modern BitChute API scraper using official endpoints"
+__description__ = "A package to scrape BitChute platform recommendations."
 
 __all__ = [
     'BitChuteAPI',
